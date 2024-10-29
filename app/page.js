a19a28de3a2c1c3e92e296616dbaad6bb71f81d4
mohammadshahrukh.digitalwolf@gmail.com <mohammadshahrukh.digitalwolf@gmail.com>
2024-10-29 22:50:26 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -7,7 +7,28 @@ export default function Home() {
     <main>
       <section>
         <div className='px-4 lg:px-8'>
-          <Splide aria-label="My Favorite Images">
+          <Splide
+            options={ {
+              rewind: true,
+              perPage: 4,
+              type   : 'loop',
+              gap   : '1rem',
+              drag   : 'free',
+              classes: {
+                arrows: 'splide__arrows your-class-arrows cds',
+                arrow : 'splide__arrow your-class-arrow',
+                prev  : 'splide__arrow--prev your-class-prev',
+                next  : 'splide__arrow--next your-class-next',
+              },
+            } }
+            aria-label="banner slider"
+          >
+            <SplideSlide>
+              <Image src="/assets/img/banner-1.jpg" alt="Image 1" width={1860} height={682}/>
+            </SplideSlide>
+            <SplideSlide>
+              <Image src="/assets/img/banner-1.jpg" alt="Image 1" width={1860} height={682}/>
+            </SplideSlide>
             <SplideSlide>
               <Image src="/assets/img/banner-1.jpg" alt="Image 1" width={1860} height={682}/>
             </SplideSlide>
